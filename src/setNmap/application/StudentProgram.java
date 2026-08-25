99197a15aa48defc310713416b025a035f71f470
Kemilly Motta <kemillymotta31@gmail.com>
2026-08-25 17:07:44 -0300

    feat: add set student exercise

--- src/setNmap/application/StudentProgram.java
+++ src/setNmap/application/StudentProgram.java
@@ -1,4 +1,36 @@
 package setNmap.application;
 
+import setNmap.entities.Student;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class StudentProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Set<Student> set = new HashSet<>();
+
+        addStudents(sc, set, "A");
+        addStudents(sc, set, "B");
+        addStudents(sc, set, "C");
+
+        System.out.println("Total students: " + set.size());
+
+        sc.close();
+    }
+
+    private static void addStudents(Scanner sc, Set<Student> set, String course) {
+        System.out.print("How many students for course " + course + ": ");
+        int quantity = sc.nextInt();
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.print("Enter ID for student " + (i + 1) + ": ");
+            int id = sc.nextInt();
+
+            set.add(new Student(id));
+        }
+    }
+
 }
--- src/setNmap/entities/Student.java
+++ src/setNmap/entities/Student.java
@@ -1,4 +1,32 @@
 package setNmap.entities;
 
+import java.util.Objects;
+
 public class Student {
+
+    private int id;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
